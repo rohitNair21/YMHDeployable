3f23c2164a84688dd767a2c7dc2dca3a46d4db5b
Stepan Susorov <steven.susorov@gmail.com>
2024-03-19 15:07:59 -0600

    Added main page

---
+++ pages/Main.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+export default function Main() {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Hello world</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
