b85b46ba61e018ae6245ed22e7bbe70aaf43b324
Stepan Susorov <steven.susorov@gmail.com>
2024-03-19 15:42:48 -0600

    Updated Main page

--- pages/Main.js
+++ pages/Main.js
@@ -1,11 +1,27 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
 
 export default function Main() {
     return(
-        <SafeAreaView>
-            <View>
-                <Text>Hello world</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inner_container}>
+                <Text style={ {color: "green"} }>Hello world!</Text>
+                <Image source={require('../assets/pepe-wave.gif')} />
             </View>
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      margin: 25,
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inner_container: {
+        display: "flex",
+        flexDirection: "column",
+        gap: 20
+    }
+});
